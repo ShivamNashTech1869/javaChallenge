dbaf843b98d875ad425267276f8c8d497ab20ed5
shivamknoldus1869 <shivam.singh@knoldus.com>
2023-03-07 12:17:18 +0530

    Add Day 0

---
+++ Day0/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scannerObject = new Scanner(System.in);
+        String literalString =  "Hello, World.";
+        String inputString  =  scannerObject.nextLine();
+        System.out.println(literalString);
+        System.out.println(inputString);
+    }
+}
